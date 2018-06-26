81dd809644c937e76890f6dc277d1184406f0e41
z0mbie42 <sylvain.kerkour@gmail.com>
2018-06-26 11:22:28 +0200

    commands: Add the CallTx function to chain call within only one transaction

--- commands.go
+++ commands.go
@@ -1,9 +1,34 @@
 package goes
 
-func Call(command Command, aggregate Aggregate, metadata Metadata) (Aggregate, Event, error) {
-	var err error
+import (
+	"github.com/jinzhu/gorm"
+)
 
+type Command interface {
+	BuildEvent() (interface{}, error)
+	Validate(interface{}) error
+}
+
+func Call(command Command, aggregate Aggregate, metadata Metadata) (Aggregate, Event, error) {
 	tx := DB.Begin()
+
+	aggregate, event, err := CallTx(tx, command, aggregate, metadata)
+	if err != nil {
+		tx.Rollback()
+		return NilAggregate{}, Event{}, err
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		tx.Rollback()
+		return NilAggregate{}, Event{}, err
+	}
+
+	return aggregate, event, nil
+}
+
+func CallTx(tx *gorm.DB, command Command, aggregate Aggregate, metadata Metadata) (Aggregate, Event, error) {
+	var err error
 
 	// if aggregate instance exists, ensure to lock the row before processing the command
 	if aggregate.GetID() != "" {
@@ -12,13 +37,11 @@ func Call(command Command, aggregate Aggregate, metadata Metadata) (Aggregate, E
 
 	err = command.Validate(aggregate)
 	if err != nil {
-		tx.Rollback()
 		return NilAggregate{}, Event{}, err
 	}
 
 	data, err := command.BuildEvent()
 	if err != nil {
-		tx.Rollback()
 		return NilAggregate{}, Event{}, err
 	}
 
@@ -31,35 +54,20 @@ func Call(command Command, aggregate Aggregate, metadata Metadata) (Aggregate, E
 
 	err = tx.Save(aggregate).Error
 	if err != nil {
-		tx.Rollback()
 		return NilAggregate{}, Event{}, err
 	}
 
 	eventDBToSave, err := event.Encode()
 	if err != nil {
-		tx.Rollback()
 		return NilAggregate{}, Event{}, err
 	}
 
 	err = tx.Create(&eventDBToSave).Error
 	if err != nil {
-		tx.Rollback()
 		return NilAggregate{}, Event{}, err
 	}
 
 	Dispatch(event)
-	//p dispatch
-
-	err = tx.Commit().Error
-	if err != nil {
-		tx.Rollback()
-		return NilAggregate{}, Event{}, err
-	}
 
 	return aggregate, event, nil
-}
-
-type Command interface {
-	BuildEvent() (interface{}, error)
-	Validate(interface{}) error
 }
