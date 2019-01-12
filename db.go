fe128d5e06aa2e72a6f1b4d1d2f4b16380fe5dc0
z0mbie42 <sylvain@kerkour.com>
2019-01-12 17:34:40 +0100

    improve store handling

--- db.go
+++ db.go
@@ -4,11 +4,19 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// Store is a type alias to avoid user to import gorm (and thus avoid version problems)
+type Store = *gorm.DB
+
 // DB should be the only one way to access the DB in your application
-var DB *gorm.DB
+var DB Store
 
 // Init initialize the db package
-func Init(db *gorm.DB) error {
+func Init(databaseURL string) error {
+	db, err := gorm.Open("postgres", databaseURL)
+	if err != nil {
+		return err
+	}
+
 	DB = db
 	return DB.DB().Ping()
 }
--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package goes
 
 // Version is the version of the goes library
-const Version = "0.17.2"
+const Version = "0.18.0"
